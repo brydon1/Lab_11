8be7cc87c73f8651271692ac7dacb17e1e0d016c
Michaela Brydon <brydon1@kenyon.edu>
2019-11-24 18:32:00 -0500

    Added function declarations for singleParent, swapSubtrees, treeSingleParent, and treeSwapSubtrees; added function definitions for treeSingleParent, treeSwapSubtrees, and singleParent

--- binaryTree.h
+++ binaryTree.h
@@ -50,6 +50,10 @@ public:
     void destroyTree();
       //Deallocates the memory space occupied by the binary tree.
       //Postcondition: root = NULL;
+    void treeSwapSubtrees();
+      // swaps all left and right subtrees of binary tree
+    int treeSingleParent();
+      // Returns number of nodes in binary tree with only one child
 
     binaryTreeType(const binaryTreeType<elemType>& otherTree); 
       //copy constructor
@@ -95,6 +99,10 @@ private:
     int leavesCount(binaryTreeNode<elemType> *p) const;
       //Function to return the number of leaves in the binary 
       //tree to which p points 
+    void swapSubtrees(binaryTreeNode<elemType> *p) const;
+      // Function to swaps all left and right subtrees of binary tree to which p points
+    int singleParent(binaryTreeNode<elemType> *p) const;
+      // Returns number of nodes in binary tree to which p points with only one child 
 };
 
     //Definition of member functions
@@ -234,6 +242,22 @@ void  binaryTreeType<elemType>::destroyTree()
     destroy(root);
 }
 
+// ADDED ***********************
+
+template <class elemType>
+void binaryTreeType<elemType>::treeSwapSubtrees() const
+{
+    swapSubtrees(root);
+}
+
+template <class elemType>
+int binaryTreeType<elemType>::treeSingleParent() const
+{
+    return singleParent(root);
+}
+
+// ********************************
+
 	//copy constructor
 template <class elemType>
 binaryTreeType<elemType>::binaryTreeType
@@ -304,6 +328,32 @@ int binaryTreeType<elemType>::leavesCount(binaryTreeNode<elemType> *p) const
          << endl;
 
     return 0;
+}
+
+// ADDED *********************************************************************
+
+template <class elemType>
+int binaryTreeType<elemType>::swapSubtrees(binaryTreeNode<elemType> *p) const
+{
+    cout << "Write the definition of the function swapSubtrees"
+         << endl;
+
+    return 0;
+}
+
+template <class elemType>
+int binaryTreeType<elemType>::singleParent(binaryTreeNode<elemType> *p) const
+{
+    numSingleParents = 0;
+    if (p != NULL)
+    {
+        if ((p->llink == NULL && p->rlink != NULL) 
+          || (p->rlink == NULL && p->llink != NULL)){
+          numSingleParents ++;
+        }
+        numSingleParents += singleParent(p->llink) + singleParent(p->rlink);
+    }
+    return numSingleParents;
 }
 
 #endif
